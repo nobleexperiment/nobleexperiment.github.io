c7a320c1b2229b1c4f54ae631fc9eef2d3d5d1b5
Al Porotesano <truth.now.forever@gmail.com>
2016-08-27 21:34:35 -0700

    Create flickr.rb

---
+++ _plugins/flickr.rb
@@ -0,0 +1,27 @@
+require 'flickraw'
+
+module Jekyll
+
+  class GeneratePhotosets < Generator
+
+    safe true
+    priority :low
+
+    def generate(site)
+    end
+
+    def generate_photosets(site)
+    end
+
+    def load_photos(photoset, site)
+    end
+
+    def generate_photo_data(photoset, site)
+    end
+
+  end
+
+  class FlickrPhoto
+  end
+
+end
